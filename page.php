368c62b7d4d4bc5ba44f96fb1cd7e0a4a640c5e0
Sergio De Bei <sergio@festina.xxx>
2018-11-21 12:47:04 +0100

    Update repo

--- page.php
+++ page.php
@@ -1,33 +1,21 @@
 <?php
 /**
  * The main template file.
- * Template Name: Page
- * Description: Page
  * @package Fresh Theme
  */
 
 get_header(); ?>
 
-	<?php
-	if ( have_posts() ) :
+	<?php 
 
-		/* Start the Loop */
-		while ( have_posts() ) : the_post();
+		/*
+		 * Include the Post-Format-specific template for the content.
+		 * If you want to override this in a child theme, then include a file
+		 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+		 */
+		get_template_part( 'template-parts/content', get_post_format() );
 
-			/*
-			 * Include the Post-Format-specific template for the content.
-			 * If you want to override this in a child theme, then include a file
-			 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-			 */
-			get_template_part( 'template-parts/content', 'page' );
+	 ?>
 
-		endwhile;
-		else :
-
-
-	endif;
-	?>
-
-<?php
+<?php 
 get_footer();
-
